55760019fd2797b6e60d80b2090e4e66078909bf
unknown <silva.gabrielmiranda@gmail.com>
2021-03-25 18:20:15 -0300

    Ignews development

---
+++ src/pages/index.tsx
@@ -0,0 +1,58 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss'
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖖 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+      <h1>Oi 2</h1>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve("price_1IZ02RAbtDmA5Jymel6txu6i", {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      currency: 'USD',
+      style: 'currency'
+    }).format(price.unit_amount / 100),
+
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
+}
